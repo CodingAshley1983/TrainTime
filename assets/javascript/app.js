0576b28ba130aa25e29c1061e1059844cd628c88
AshleyH <ladynirvana@hotmail.com>
2018-11-24 13:48:13 -0700

    almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -48,35 +48,71 @@
 
     dataRef.ref().on("child_added", function(childSnapshot){
         console.log(childSnapshot.val()); 
-        var rowButton = childSnapshot.val().id;
+       
         var trainName = childSnapshot.val().name; 
         var destination = childSnapshot.val().destination; 
         var frequency= childSnapshot.val().frequency; 
         var firstTrain= childSnapshot.val().time;
+
+       
+
         $(".tbody").append(
             $("<tr>").append(
                 $("<td>").text(trainName), 
                 $("<td>").text(destination),
                 $("<td>").text(frequency),
                 $("<td>").text(firstTrain),
-                $("<td>").text(""),
-                $("<td>").append( $('<button/>', {
-                        text: 'delete',
-        
-                        id: 'btn_clear',
-                        ServerValue: "data-location",
-                        // click: clearButton
-                }
+                $("<td>").text("")
+               
                 
             ))
 
-        ));
+         });
 
-            //    function clearButton(){
-            //      $("<tr>").hide();
-            //    }
-            //    $("#btn_clear").on("click", clearButton){
-            //    console.log("button worked")
-            //    }
-    })
-  
+    
+
+            //Want to display current time and have it auto-refresh every minute
+var datetime = null,
+date = null;
+
+var update = function () {
+date = moment(new Date())
+datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+function calculate(sv) {
+
+    var firstTime= sv.startTime
+    var tFrequency = sv.frequency;
+    
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    
+    // Minute Until Train
+    var tMinAway = tFrequency - tRemainder;
+    
+    // Next Train
+    var nxtTrain = moment().add(tMinAway, "minutes");
+    console.log("Minutes away:", nxtTrain);
+    
+    };
+
+$(document).ready(function(){
+datetime = $('#current-time')
+update();
+setInterval(update, 1000);
+});
+
+
+
+
+
